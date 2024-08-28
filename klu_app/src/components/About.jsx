9a9b476db7a9c19c8d254329d8efe6055633421c
Inforable <hasri.fayadh@gmail.com>
2024-08-29 00:03:57 +0700

    delete flask_app

--- klu_app/src/components/About.jsx
+++ klu_app/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div id='about' className="p-5 lg:pt-3 mt-10 flex justify-center items-center">
       <div className="w-full lg:w-3/5 flex flex-col items-center">
         <h1 className="text-center font-bold text-navy text-5xl mb-8">About</h1>
-        {/* Baris Pertama untuk Gambar */}
+        {/* Baris Pertama untuk gambar */}
         <div className="w-full flex justify-center mb-8 -mt-12">
           <img
             src={logo}
@@ -15,7 +15,7 @@ const About = () => {
             width={300}
           />
         </div>
-        {/* Baris Kedua untuk Teks */}
+        {/* Baris Kedua untuk teks */}
         <div className="w-full flex flex-col justify-center items-center text-center -mt-10">
           <p className="text-lg text-navy">
             <span className="text-navy font-semibold">Klu</span> adalah aplikasi mobile yang dibuat untuk memudahkan komunikasi bagi tunawicara dan membantu pembelajaran bahasa isyarat bagi masyarakat yang lain. Aplikasi ini berisi fitur yang interaktif untuk mengenali, mempelajari, dan penggunaan bahasa isyarat dalam situasi tertentu dengan memanfaatkan computer vision.
